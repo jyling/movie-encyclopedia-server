7a926ad5242ff7c43cbce0fa085fe56e33ab0326
Samuel Ling <samuel.ling977@gmail.com>
2021-05-13 15:26:39 +0800

    added class-validator to validate class

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
+import { BadRequestException, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({
+    exceptionFactory: (errors) => {
+      const formattedError = errors.map(error => {
+        var obj = {};
+        obj[error.property] = Object.values(error.constraints).length > 0 ? Object.values(error.constraints)[0] : ""
+        return obj;
+      })
+      throw new BadRequestException(formattedError)
+    },
+  }));
   await app.listen(3000);
 }
 bootstrap();
