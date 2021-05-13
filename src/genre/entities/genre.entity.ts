c039b01142013e5b953e96a84a90ad64bfa2d562
Samuel Ling <samuel.ling977@gmail.com>
2021-05-13 08:37:14 +0800

    changed the genre entity

--- src/genre/entities/genre.entity.ts
+++ src/genre/entities/genre.entity.ts
@@ -2,6 +2,12 @@ import { ObjectType, Field, Int } from '@nestjs/graphql';
 
 @ObjectType()
 export class Genre {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @Field(() => Int, { description: 'id of genre' , nullable : true})
+  id: number;
+
+  @Field(() => String, { description: 'name of genre' })
+  name: string;
+
+  @Field(() => String, { description: 'description of genre' })
+  description: string;
 }
