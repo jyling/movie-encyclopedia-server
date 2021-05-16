1d0a667367d06e07570236d1428ec866178cb827
Samuel Ling <samuel.ling977@gmail.com>
2021-05-16 14:09:50 +0800

    added relationship to the director and writer

--- src/people/entities/people.entity.ts
+++ src/people/entities/people.entity.ts
@@ -1,4 +1,6 @@
 import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { MovieDirector } from 'src/movie-director/entities/movie-director.entity';
+import { MovieWriter } from 'src/movie-writer/entities/movie-writer.entity';
 
 @ObjectType()
 export class People {
@@ -7,4 +9,10 @@ export class People {
 
   @Field(() => String, { description: 'name of the people' })
   name: string;
+
+  @Field(() => [MovieDirector])
+  MovieDirector: MovieDirector[]
+  
+  @Field(() => [MovieWriter])
+  MovieWriter: MovieWriter[]
 }
